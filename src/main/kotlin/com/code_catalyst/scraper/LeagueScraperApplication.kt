ca64843e50a41220d4fcc92bfc9fae412b4d81b5
John Parkhurst <jparkhurst120@gmail.com>
2024-09-11 16:26:58 -0400

    INIT

---
+++ src/main/kotlin/com/code_catalyst/scraper/LeagueScraperApplication.kt
@@ -0,0 +1,11 @@
+package com.code_catalyst.scraper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LeagueScraperApplication
+
+fun main(args: Array<String>) {
+	runApplication<LeagueScraperApplication>(*args)
+}
